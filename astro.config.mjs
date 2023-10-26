a4c95e7eea4a00a80457556aaa888fac0e0d6f0a
c3qo <c3qo@c3qo.com>
2023-10-26 13:57:27 -0700

    blog-3: moving away from Getx

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,6 +28,7 @@ export default defineConfig({
 					label: 'Inbox - Uncategorized',
 					items: [
 						{  label: 'Flutter: Pre-signed S3 Url with Firebase Auth', link: '/2-aws-s3-presigned-url-for-get-put' },
+						{  label: 'Flutter: From Getx to Riverpod/GoRouter/Others', link: '/3-migrate-from-getx-to-riverpod-go-router-others' },
 					],
 				},
 			],
