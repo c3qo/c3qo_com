fd2bc4028d57de640228ddc1c9c1f319280493ee
c3qo <c3qo@c3qo.com>
2023-10-26 13:57:27 -0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://c3qo.com',
 	integrations: [
+		sitemap(),
 		starlight({
 			title: 'c3qo - a cloud',
 			social: {
